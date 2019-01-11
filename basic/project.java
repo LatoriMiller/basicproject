e4c56b9daa13b8b1137e54c3c6bbaca2bcbaace1
LatoriMiller <latori.miller@gmail.com>
2019-01-11 13:23:23 -0500

    edit Project.java file

--- basic/project.java
+++ basic/project.java
@@ -1,10 +1,9 @@
 package basic;
 
-public class project {
+public class Project {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		System.out.println("My name is Latori");
 		System.out.println("I am building this project all on my own");
 	}
