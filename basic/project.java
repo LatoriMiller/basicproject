1dc466ef7c54b501342f99be7321f27bc35d33bf
LatoriMiller <latori.miller@gmail.com>
2019-01-11 11:52:56 -0500

    first commit

---
+++ basic/project.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My name is Latori");
+		System.out.println("I am building this project all on my own");
+	}
+
+}
